8428f5c752f311a2d28f0230227290879f1225ca
Daniel Stollnberger <daniiistolliii@gmail.com>
2025-04-27 12:22:43 +0200

    finished off html assets without responsive & began with logic for creating a task

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const url = 'https://join-id-default-rtdb.europe-west1.firebasedatabase.app/';
-
-
+let priorityData = [];
+let subtaskElements = [];
 
 function firstInit() {
     let overlay = document.getElementById('first-load');
@@ -50,6 +50,8 @@ async function login() {
 async function init(site) {
     if (site == 'summary') {
         summaryInit();
+    } else if (site == 'dashboard') {
+        dashboardInit();
     }
 }
 
@@ -117,7 +119,7 @@ async function summaryInit() {
     let currentUserResponse = await fetch(url + 'user/' + userData + '.json');
     let currentUser = await currentUserResponse.json();
     if (userData == 'guest') {
-        guestSummary('summary');
+        guestSummary();
     } else {
         let fullName = currentUser.name;
         let [firstName, lastName] = fullName.split(" ");
@@ -125,12 +127,42 @@ async function summaryInit() {
     }
 }
 
+async function dashboardInit() {
+    renderToDos();
+}
+
+async function renderToDos() {
+    let toDoContent = document.getElementById('to-do');
+    let responseToDo = await fetch(url + 'tasks/to-do.json');
+    let dataToDo = await responseToDo.json();
+    let tasks = Object.keys(dataToDo).map((key) => [key, dataToDo[key]]);
+
+for (let i = 0; i < tasks.length; i++) {
+   toDoContent.innerHTML += `<div class="tasks">
+                                <h3 class="${tasks[i][1]['category']}">${tasks[i][1]['category'].replace('-',' ')}</h3>
+                                <h4 class="mt-30 mb-10">${tasks[i][1].title}</h4>
+                                <span class="task-span">${tasks[i][1].description}</span>
+                                <div class="dashboard-subtask-div fs-14 ml-10" style='white-space: nowrap;'>
+                                    <progress value="0" max="100"></progress>
+                                    <span>0/${tasks[i][1].subtasks.length} Subtasks</span>
+                                </div>
+                                <div class="assigned-div">
+                                    <div>DS</div>
+                                    <img src="./assets/icons/priority-medium.png" alt="">
+                                </div>
+                            </div>`;
+}
+
+    console.log(dataToDo);
+    console.log(tasks);
+}
+
 function checkName(name) {
     if (name.split(/\s+/).length == 2) {
         return true;
     } else {
-       alert('Enter your full name please!');
-       return false;
+        alert('Enter your full name please!');
+        return false;
     }
 }
 
@@ -141,11 +173,73 @@ async function userShort() {
     let currentUserResponse = await fetch(url + 'user/' + userData + '.json');
     let currentUser = await currentUserResponse.json();
     if (userData == 'guest') {
-       userShort.innerHTML = 'G';
+        userShort.innerHTML = 'G';
     } else {
         let fullName = currentUser.name;
         let [firstName, lastName] = fullName.split(" ");
         let initials = firstName[0] + lastName[0];
         userShort.innerHTML = initials.toUpperCase();
     }
+}
+
+async function createTask() {
+    let taskTitle = document.getElementById('task-title').value;
+    let dueDate = document.getElementById('due-date').value;
+    let category = document.getElementById('category-selection').value;
+    let description = document.getElementById('description').value;
+    let assignedTo = document.getElementById('assigned-to').value;
+    let taskData = { title: taskTitle, "due-date": dueDate, priority: priorityData.toString(), category, description, "assigned-to": assignedTo, subtasks: subtaskElements };
+    await fetch(url + 'tasks/to-do.json', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(taskData)
+    });
+    clearTask();
+}
+
+function enableCreateTaskButton() {
+    let taskTitle = document.getElementById('task-title').value;
+    let dueDate = document.getElementById('due-date').value;
+    let createButton = document.getElementById('create-task-button');
+    let category = document.getElementById('category-selection').value
+
+    if (taskTitle.length > 0 && dueDate.length > 0 && priorityData.length > 0 && (category === 'technical-task' || category === 'user-story')) {
+        createButton.disabled = false;
+    } else {
+        createButton.disabled = true;
+    }
+}
+
+function addInfoToTask(priority) {
+    priorityData.splice(0);
+    priorityData.push(priority);
+    enableCreateTaskButton();
+}
+
+function clearTask() {
+    document.getElementById('task-title').value = '';
+    document.getElementById('due-date').value = '';
+    document.getElementById('category-selection').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('assigned-to').value = '';
+    document.getElementById('subtask-content').innerHTML = '';
+    document.getElementById('subtask-input').value = '';
+    document.getElementById("urgent").checked = false;
+    document.getElementById("medium").checked = false;
+    document.getElementById("low").checked = false;
+    priorityData = [];
+    subtaskElements = [];
+};
+
+function addSubtask() {
+    let subtaskInput = document.getElementById('subtask-input').value
+    subtaskElements.push(subtaskInput);
+
+    let subtaskContent = document.getElementById('subtask-content');
+    subtaskContent.innerHTML = '';
+    for (let j = 0; j < subtaskElements.length; j++) {
+
+        subtaskContent.innerHTML += `<li class='added-subtask'>${subtaskElements[j]}</li>`;
+    }
+    document.getElementById('subtask-input').value = '';
 }
